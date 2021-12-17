26f4f2404e2c5886a0b0e2566bb9ac4de0348b6d
ChaklunIvan <mr.skratt@gmail.com>
2021-12-17 12:35:38 +0200

    delete folder domain

--- PersonalBlog.Domain/Responses/AuthenticatedUserResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace PersonalBlog.Domain.Responses
-{
-    public class AuthenticatedUserResponse
-    {
-        public string? AccessToken { get; set; }
-        public string RefreshToken { get; set; }
-    }
-}
